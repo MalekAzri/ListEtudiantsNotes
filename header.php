e4c42abfb2b1297b228165e02270e06cfcb20db9
Malek <malek.azri@insat.ucar.tn>
2025-04-05 21:36:31 +0100

    un header pour ne pas reecrire à chaque fois le header html

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title><?php echo $title ?></title>
+  <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">-->
+ <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css"> 
+  
+
+</head>
+
+<body class="bg-light">
