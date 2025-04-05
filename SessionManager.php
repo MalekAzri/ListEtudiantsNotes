bbfabffbc8415277254696509eddc7226c600553
Malek <malek.azri@insat.ucar.tn>
2025-04-05 21:27:09 +0100

    fonction existSession pour verifier si la session existe deja

--- SessionManager.php
+++ SessionManager.php
@@ -1,8 +1,17 @@
 <?php
+include_once 'Session.php';
 class SessionManager{
     public static array $sessions=[];
     public static  function ajouterSession(Session $session):void{
         self::$sessions[]=$session;
     }
-    
+    public static function existSession(Session $session):bool{
+        foreach(self::$sessions as $s){
+            if($s->getId()==$session->getId()){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
