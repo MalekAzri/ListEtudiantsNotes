18ff63e118c68a4620310b7ed2bc91d44de07ddb
Malek <malek.azri@insat.ucar.tn>
2025-04-05 22:18:30 +0100

    j'ai enlevé l'entete html puisque j'ai tout mis dans header.php

--- index.php
+++ index.php
@@ -5,26 +5,15 @@ include "Etudiant.php";
 session_start();
 
 //session_destroy();
+$title="Formulaire Étudiant";
 
 
+
+
+
+include "header.php";
 
 ?>
-
-
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="UTF-8">
-  <title>Formulaire Étudiant</title>
-  <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">-->
- <link rel="stylesheet" href="node_modules\bootstrap\dist\css\bootstrap.min.css"> 
-  
-
-</head>
-
-<body class="bg-light">
-
-
 <div class="container mt-5">
   <h2 class="mb-4">Saisie des informations d’un étudiant</h2>
   <form  method="post">
